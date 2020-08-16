203c29545da707072fd47734bae9ae09e28f03e6
oleg90990 <oleg909902@yandex.ru>
2020-08-17 00:58:59 +0300

    first commit

---
+++ src/components/Container/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <b-container>
+    <slot></slot>
+  </b-container>
+</template>
+
+<script lang="ts" src="./index.ts" />
---
+++ src/pages/Home/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container">
+      Home
+  </div>
+</template>
+
+<script lang="ts" src="./index.ts" />
